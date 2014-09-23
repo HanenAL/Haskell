1535b30d9c8af2bcd879497c7904862fe6cdf404
Eriuo <xeriuo@hotmail.com>
2014-09-23 11:35:10 +0200

    Update Sudoku.hs

--- Sudoku/Sudoku.hs
+++ Sudoku/Sudoku.hs
@@ -5,9 +5,24 @@ import Test.QuickCheck
 -----------------------------------------------------------------------------
 
 data Sudoku = Sudoku [[Maybe Int]]
+   deriving(Eq, Show)
 
 rows :: Sudoku -> [[Maybe Int]]
 rows (Sudoku rs) = rs
+
+example :: Sudoku
+example =
+    Sudoku
+      [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
+      , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
+      , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
+      , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
+      , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
+      , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
+      , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+      , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
+      , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+      ]
 
 -- A1:
 -- Creates a list of lists containing 9 "nothing" each, (map).
@@ -16,10 +31,25 @@ allBlankSudoku = Sudoku ([ [ Nothing | x <- [1..9] ] | x <- [1..9] ])
 
 -- A2:
 isSudoku :: Sudoku -> Bool
-isSudoku sudoku = undefined
+isSudoku sudoku = sizeOK (rows sudoku)
+
+sizeOK :: [[Maybe Int]] -> Bool
+sizeOK xs | size xs /= 9 = False
+sizeOK xs | otherwise    = and [ (size (selectRow xs x )) == 9 | x <- [0..8] ]
+
+selectRow :: [[Maybe Int]] -> Int -> [Maybe Int]
+selectRow (x:_) 0  = x
+selectRow (x:xs) n = selectRow xs (n-1)
+
+size :: [a] -> Int
+size []     = 0
+size (x:xs) = 1 + size xs
 
 -- A3:
 isSolved :: Sudoku -> Bool
 isSolved = undefined
+
+-- Add function: check every row if they have any "nothing".
+-- Add function: Adds the boolean value of all rows.
 
 -----------------------------------------------------------------------------
