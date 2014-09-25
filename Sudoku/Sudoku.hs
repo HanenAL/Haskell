61af3495c808ef304181911aeaf748e319b8c8d4
RobinEdquist <r.edquist@hotmail.com>
2014-09-25 17:13:35 +0200

    Unfinished shithead
    *added isSolved function
    *

--- Sudoku/Sudoku.hs
+++ Sudoku/Sudoku.hs
@@ -1,6 +1,6 @@
 module Sudoku where
 
-import Test.QuickCheck
+--import Test.QuickCheck
 
 -----------------------------------------------------------------------------
 
@@ -13,6 +13,20 @@ rows (Sudoku rs) = rs
 example :: Sudoku
 example =
     Sudoku
+      [ [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      , [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]
+      ]
+
+example2 :: Sudoku
+example2 =
+    Sudoku
       [ [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing]
       , [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing]
       , [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing]
@@ -23,6 +37,7 @@ example =
       , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
       , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
       ]
+
 
 -- A1:
 -- Creates a list of lists containing 9 "nothing" each, (map).
@@ -41,13 +56,16 @@ selectRow :: [[Maybe Int]] -> Int -> [Maybe Int]
 selectRow (x:_) 0  = x
 selectRow (x:xs) n = selectRow xs (n-1)
 
+checkRow :: [Maybe Int] -> Bool
+checkRow row = and [ Nothing /= x | x <- row ]
+
 size :: [a] -> Int
 size []     = 0
 size (x:xs) = 1 + size xs
 
 -- A3:
 isSolved :: Sudoku -> Bool
-isSolved = undefined
+isSolved sudoku = and [ checkRow (selectRow (rows sudoku) x) | x <- [0..8] ]
 
 -- Add function: check every row if they have any "nothing".
 -- Add function: Adds the boolean value of all rows.
