73f530122d914100d928bb072ecfe8433a41fc0e
Eriuo <xeriuo@hotmail.com>
2014-10-16 17:03:36 +0200

    Update Expr.hs

--- Expr/Expr.hs
+++ Expr/Expr.hs
@@ -12,6 +12,7 @@ data Expr = Num Double
           | X
           | Sin Expr
           | Cos Expr
+    deriving(Eq)
 
 -----------------------------------------------------------------------------
 
@@ -44,34 +45,22 @@ eval (Cos n) x   = cos (eval n x)
 
 type Parser a = String -> Maybe (a,String)
 
--- `number` parses a number
-number :: Parser Double -- )
+number :: Parser Double
+number ('-':s) = fmap negate' (number s)
 number s = listToMaybe(reads s)
---number ('-':s)           = fmap negate' (number s)
 
 negate' :: (Double,String) -> (Double,String)
 negate' (n,s) = (-n,s)
 
--- `num` parses a numeric expression
 num :: Parser Expr
 num s = case number s of
     Just (n,s') -> Just (Num n, s')
     Nothing     -> Nothing
 
--- * an expression is a '+'-chain of terms
--- * a term is a '*'-chain of factors
 expr, term :: Parser Expr
 expr = chain term   '+' Add
 term = chain factor '*' Mul
 
--- `chain p op f s1` parsers a "chain" of things.
---
---   * The things are parsed by the parser `p`.
---   * The things are separated by the symbol `op`.
---   * The things are combined by the function `f`.
---
--- For example "12+23+1+172" is a chain of numbers, separated by the symbol '+'.
--- chain string int "+" Add
 chain :: Parser a -> Char -> (a -> a -> a) -> Parser a
 chain p op f s1 =
   case p s1 of
@@ -82,21 +71,57 @@ chain p op f s1 =
                      _              -> Just (a,s2)
     Nothing     -> Nothing
 
--- `factor` parses a "factor": either a number or an expression surrounded by
--- parentheses
 factor :: Parser Expr
-factor ('(  ':s) =
+factor ('(':s) =
    case expr s of
       Just (a, ')':s1) -> Just (a, s1)
       _                -> Nothing
+factor ('s':'i':'n':s) =
+   case factor s of
+      Just (a, s1) -> Just (Sin a, s1)
+      _            -> Nothing
+factor ('c':'o':'s':s) =
+   case factor s of
+      Just (a, s1) -> Just (Cos a, s1)
+      _            -> Nothing
+factor ('x':s) =
+   case factor s of
+      Just (a, s1) -> Just (X, s1)
+      _            -> Nothing
 factor s = num s
-
-
 
 readExpr :: String -> Maybe Expr
 readExpr s =
   case expr s of
     Just (a,"") -> Just a
     _           -> Nothing
+
+-----------------------------------------------------------------------------
+
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr expr = fromJust (readExpr $ showExpr expr) == expr
+
+instance Arbitrary Expr where
+  arbitrary = sized arbExpr
+
+arbExpr :: Int -> Gen Expr
+arbExpr s =
+  frequency [ (1, do n <- arbitrary
+                     return (Num n))
+            , (s, do a <- arbExpr s'
+                     b <- arbExpr s'
+                     return (Add a b))
+            , (s, do a <- arbExpr s'
+                     b <- arbExpr s'
+                     return (Mul a b))
+            , (s, do n <- arbExpr s'
+                     return (Sin n))
+            , (s, do n <- arbExpr s'
+                     return (Cos n))
+            , (s, do n <- arbExpr s'
+                     return (Sin n))
+            ]
+ where
+  s' = s `div` 2
 
 -----------------------------------------------------------------------------
